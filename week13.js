fbba544ad3a4260e9fd99884d1ba0c6b5ad82e4c
rkvrpl <rkvrpl@icloud.com>
2023-11-29 17:34:20 +0300

    done a half of work

--- week13.js
+++ week13.js
@@ -1,44 +1,81 @@
 //Задание 1
 // Создайте переменную currentDate и присвойте ей текущую дату. Значение выведите в консоль.
+const currentDate = new Date();
+console.log(currentDate);
 
 //Задание 2
 // Создайте переменную currentYear и присвойте ей текущий год. Значение выведите в консоль.
+const currentYear = currentDate.getFullYear();
+console.log(currentYear);
 
 //Задание 3
 // Создайте переменную currentMonth и присвойте ей текущий месяц (от 0 до 11, где 0 - январь, 11 - декабрь). Значение выведите в консоль.
+const currentMonth = currentDate.getMonth();
+console.log(currentMonth);
 
 //Задание 4
 // Создайте переменную currentDay и присвойте ей текущий день месяца. Значение выведите в консоль.
+const currentDay = currentDate.getDate();
+console.log(currentDay);
 
 //Задание 5
 // Создайте переменную birthday и присвойте ей дату вашего дня рождения в текущем году. Значение выведите в консоль.
+const birthday = new Date(2023, 11, 21);
+console.log(birthday);
 
 //Задание 6
 // Создайте переменную futureDate и присвойте ей будущую дату (например, через 1 месяц и 10 дней от текущей даты). Значение выведите в консоль.
+const futureDate = new Date();
+futureDate.setDate(futureDate.getDate() + 33);
+console.log(futureDate);
 
 //Задание 7
 // Выведите в консоль разницу в днях между futureDate и currentDate
+const diff = futureDate -currentDate;
+const result = new Date(diff);
+console.log(Math.round(result.getDate()));
 
 //Задание 8
 // Создайте переменную pastDate и присвойте ей прошедшую дату (например, 5 дней назад от текущей даты). Значение выведите в консоль.
 
+const pastDate = new Date();
+pastDate.setDate(pastDate.getDate() - 5);
+console.log(pastDate);
+
 //Задание 9
 // Выведите в консоль разницу в днях между currentDate и pastDate
+
+const diffDays = futureDate - pastDate;
+const resultDays = new Date(diffDays);
+console.log(Math.round(resultDays.getDate()));
 
 //Задание 10
 // Создайте переменную nextWeek и присвойте ей дату следующей недели. Значение выведите в консоль.
 
+const nextWeek = new Date();
+nextWeek.setDate(nextWeek.getDate() + 7)
+console.log(nextWeek);
+
 //Задание 11
 // Выведите в консоль день недели для nextWeek
+console.log(nextWeek.getDay());
 
 //Задание 12
 // Создайте переменную futureYear и присвойте ей значение текущего года плюс 5. Значение выведите в консоль.
+const futureYear = new Date();
+futureYear.setFullYear(futureYear.getFullYear() + 5)
+console.log(futureYear);
 
 //Задание 13
 // Создайте переменную futureDateInFutureYear и присвойте ей дату вашего дня рождения в будущем году (futureYear). Значение выведите в консоль.
+const futureDateInFutureYear = new Date(2024, 11, 21);
+console.log(futureDateInFutureYear);
 
 //Задание 14
 // Выведите в консоль разницу в годах между futureYear и текущим годом
+const diffYears = futureYear.getFullYear() - currentDate.getFullYear();
+const resultYears = new Date(diffDays);
+console.log(Math.round(resultYears.getDate()));
 
 //Задание 15
 // Используя метод Date.parse(), создайте новый объект date и преобразуйте переменную strDate в дату.
@@ -87,8 +124,8 @@ const number = 231;
 
 //Задание 28
 // Используя цикл switch case, напишите программу, которая принимает на вход символ направления (N, S, E, W) и выводит в консоль полное название этого направления (N - "Север", S - "Юг", E - "Восток", W - "Запад").
-let direction = 'W';
-let fullName;
+// let direction = 'W';
+// let fullName;
 
-switch (direction) {
-}
+// switch (direction) {
+// }
